33129aa2c7ffaba9b443b888beb4cee80da02996
AchantaHarshini <22MH1A0545@acoe.edu.in>
2026-03-11 16:24:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -204,5 +204,6 @@ if __name__ == "__main__":
 
     print(f"🚀 IDS Backend running on port {port}")
 
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=False)
+
 
