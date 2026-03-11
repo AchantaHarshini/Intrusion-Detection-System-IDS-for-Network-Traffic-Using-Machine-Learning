c8d2d202f1e800f6f1448fe8040b92948efbf7d0
AchantaHarshini <22MH1A0545@acoe.edu.in>
2026-03-11 15:45:28 +0530

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import pandas as pd
 import os
@@ -171,6 +171,7 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     print(f"🚀 IDS Backend running on port {port}")
     app.run(host="0.0.0.0", port=port)
+
 
 
 
